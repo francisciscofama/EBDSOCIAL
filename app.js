0bd0218fd0d639ac52710b7de3fbfd3974cea73b
franciscofama <franciscomarquesafonso@gmail.com>
2025-10-20 01:35:56 +0100

    first commit

--- app.js
+++ app.js
@@ -22,6 +22,49 @@ const posts = [
     text: "🙌 Deus tem grandes planos para ti!",
     date: "16 de Outubro, 2025",
   },
+
+  {
+    user: "Igreja Betel",
+    avatar: "https://i.pravatar.cc/150?img=12",
+    image: "https://picsum.photos/500/300?random=1",
+    text: "🎶 Louvai ao Senhor, porque Ele é bom! 🙌",
+    date: "18 de Outubro, 2025",
+  },
+  {
+    user: "EBD Central",
+    avatar: "https://i.pravatar.cc/150?img=23",
+    image: "https://picsum.photos/500/300?random=2",
+    text: "📖 Tema da semana: 'Andar na luz' 💡",
+    date: "17 de Outubro, 2025",
+  },
+  {
+    user: "Juventude Cristã",
+    avatar: "https://i.pravatar.cc/150?img=45",
+    image: "https://picsum.photos/500/300?random=3",
+    text: "🙌 Deus tem grandes planos para ti!",
+    date: "16 de Outubro, 2025",
+  },
+  {
+    user: "Igreja Betel",
+    avatar: "https://i.pravatar.cc/150?img=12",
+    image: "https://picsum.photos/500/300?random=1",
+    text: "🎶 Louvai ao Senhor, porque Ele é bom! 🙌",
+    date: "18 de Outubro, 2025",
+  },
+  {
+    user: "EBD Central",
+    avatar: "https://i.pravatar.cc/150?img=23",
+    image: "https://picsum.photos/500/300?random=2",
+    text: "📖 Tema da semana: 'Andar na luz' 💡",
+    date: "17 de Outubro, 2025",
+  },
+  {
+    user: "Juventude Cristã",
+    avatar: "https://i.pravatar.cc/150?img=45",
+    image: "https://picsum.photos/500/300?random=3",
+    text: "🙌 Deus tem grandes planos para ti!",
+    date: "16 de Outubro, 2025",
+  },
 ];
 
 function carregarPosts() {
